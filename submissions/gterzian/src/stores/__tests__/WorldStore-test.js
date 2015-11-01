6c626760ab6dd3790838b27096f4fbe7b6739ea8
Gregory <gterzian@users.noreply.github.com>
2015-11-01 14:06:48 +0800

    implement reduce

--- submissions/gterzian/src/stores/__tests__/WorldStore-test.js
+++ submissions/gterzian/src/stores/__tests__/WorldStore-test.js
@@ -1,8 +1,10 @@
 jest.dontMock('../WorldStore');
+jest.dontMock('../../dispatcher/Dispatcher');
 jest.dontMock('immutable');
 
 const WorldStore = require('../WorldStore');
 const Map = require('immutable').Map;
+const Dispatcher = require('../../dispatcher/Dispatcher');
 
 describe('Stores: WorldStore', () => {
 
@@ -13,6 +15,15 @@ describe('Stores: WorldStore', () => {
       expect(state.get("name")).toEqual("");
       expect(Map.isMap(state)).toEqual(true);
     });
+  });
 
+  describe('WorldStore.reduce', () => {
+    it('Should reduce the sate when receiving world actions', () => {
+      var state = WorldStore.reduce(WorldStore.getInitialState(), {type: 'NEW_WORLD', id: 123, name: 'earth'});
+      //console.log(state)
+      expect(state.get("id")).toEqual(123);
+      expect(state.get("name")).toEqual("earth");
+      expect(Map.isMap(state)).toEqual(true);
+    });
   });
 });
