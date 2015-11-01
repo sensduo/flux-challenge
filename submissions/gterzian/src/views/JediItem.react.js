89e408919d9c5991afe9fdfdc134182866f91cb5
Gregory <gterzian@users.noreply.github.com>
2015-11-02 00:15:21 +0800

    setting color to red when home

--- submissions/gterzian/src/views/JediItem.react.js
+++ submissions/gterzian/src/views/JediItem.react.js
@@ -3,11 +3,21 @@ import React, {Component} from 'react';
 export default class JediItem extends Component {
 
   render() {
-    return (
-      <li className="css-slot">
-        <h3>{this.props.name}</h3>
-        <h6>Homeworld: {this.props.homeworld}</h6>
-      </li>
-    );
+    if(this.props.isHome) {
+      return (
+        <li className='css-slot'>
+          <h3 style={{color:'red'}}>{this.props.name}</h3>
+          <h6>Homeworld: {this.props.homeworld}</h6>
+        </li>
+      );
+    }
+    else {
+      return (
+        <li className='css-slot'>
+          <h3>{this.props.name}</h3>
+          <h6>Homeworld: {this.props.homeworld}</h6>
+        </li>
+      );
+    }
   }
 };
