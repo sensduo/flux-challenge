4a0ec40fd49ca7e36a06a1c61446bc57cf98603b
Gregory <gterzian@users.noreply.github.com>
2015-11-01 17:37:55 +0800

    a container and a bunch of views

---
+++ submissions/gterzian/src/views/JediScroll.react.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class JediScroll extends Component {
+
+  render() {
+    <div class="css-scroll-buttons">
+      <button class="css-button-up"></button>
+      <button class="css-button-down"></button>
+    </div>
+    );
+  }
+};
