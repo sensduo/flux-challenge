0292660862e146f6c1a1a97b6d7f587ce4de7612
Gregory <gterzian@users.noreply.github.com>
2015-11-02 02:43:22 +0800

    update tests

--- submissions/gterzian/src/actions/__tests__/JediActions-test.js
+++ submissions/gterzian/src/actions/__tests__/JediActions-test.js
@@ -3,27 +3,11 @@ var JediActions = require('../JediActions');
 
 describe('Actions: JediActions', () => {
 
-  describe('JediActions.scrollUp', () => {
-    it('Should return an Action of type SCROLL_UP', () => {
-      var scrollUpAction = JediActions.scrollUp();
-      expect(scrollUpAction.type).toEqual('SCROLL_UP');
-    });
-
-    it('Should return an Action with the id passed to it under "from"', () => {
-      var scrollUpAction = JediActions.scrollUp("test");
-      expect(scrollUpAction.from).toEqual("test");
+  describe('JediActions.newJedi', () => {
+    it('Should return an Action of type NEW_JEDI', () => {
+      var scrollUpAction = JediActions.newJedi();
+      expect(scrollUpAction.type).toEqual('NEW_JEDI');
     });
   });
 
-  describe('JediActions.scrollDown', () => {
-    it('Should return an Action of type SCROLL_DOWN', () => {
-      var scrollDownAction = JediActions.scrollDown();
-      expect(scrollDownAction.type).toEqual('SCROLL_DOWN');
-    });
-
-    it('Should return an Action with the id passed to it under "from"', () => {
-      var scrollDownAction = JediActions.scrollDown('test');
-      expect(scrollDownAction.from).toEqual('test');
-    });
-  });
 });
