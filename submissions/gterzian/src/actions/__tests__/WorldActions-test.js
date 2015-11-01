1bb9dfe9af3b5aac146c787ccc5b3a38734e7176
Gregory <gterzian@users.noreply.github.com>
2015-11-01 13:48:19 +0800

    implement reduce method of WorldStore, change WorldAction

--- submissions/gterzian/src/actions/__tests__/WorldActions-test.js
+++ submissions/gterzian/src/actions/__tests__/WorldActions-test.js
@@ -6,12 +6,17 @@ describe('Actions: WorldActions', () => {
   describe('WorldActions.newWorld', () => {
     it('Should return an Action of type NEW_WORLD', () => {
       var newWorldAction = WorldActions.newWorld();
-      expect(newWorldAction.Type).toEqual('NEW_WORLD');
+      expect(newWorldAction.type).toEqual('NEW_WORLD');
     });
 
     it('Should return an Action with the id of the new world', () => {
       var newWorldAction = WorldActions.newWorld('test_world');
-      expect(newWorldAction.worldId).toEqual('test_world');
+      expect(newWorldAction.id).toEqual('test_world');
+    });
+
+    it('Should return an Action with the name of the new world', () => {
+      var newWorldAction = WorldActions.newWorld('test_world', 'earth');
+      expect(newWorldAction.name).toEqual('earth');
     });
   });
 });
