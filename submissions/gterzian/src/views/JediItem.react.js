7710e46c99b1d4e5e899f44dda345ce6ee059381
Gregory <gterzian@users.noreply.github.com>
2015-11-01 17:47:30 +0800

    fixing syntax and imports

--- submissions/gterzian/src/views/JediItem.react.js
+++ submissions/gterzian/src/views/JediItem.react.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class JediItem extends Component {
 
   render() {
-    <li class="css-slot">
+    (<li class="css-slot">
       <h3>{this.props.name}</h3>
       <h6>Homeworld: {this.props.homeworld}</h6>
     </li>
