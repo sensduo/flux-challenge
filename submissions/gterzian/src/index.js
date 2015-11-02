d227212b0b5ebb3a311871245be6d8d28e1bf2f0
Gregory <gterzian@users.noreply.github.com>
2015-11-02 22:28:49 +0800

    fetching initial 5 jedis

--- submissions/gterzian/src/index.js
+++ submissions/gterzian/src/index.js
@@ -3,7 +3,12 @@ import React from 'react';
 import App from './views/App.react';
 import webApi from './utils/web-api';
 import WorldActions from './actions/WorldActions'
+import JediActions from './actions/JediActions'
 
 React.render(<App/>, document.getElementById('app'));
 webApi.openWs();
-webApi.getJedi('http://localhost:3000/dark-jedis/3616');
+webApi.getJedi('http://localhost:3000/dark-jedis/3616').then((jedi) => {
+  webApi.getJedi(jedi.apprentice.url, 'Apprentice').then(() => {
+    webApi.getJedi(jedi.master.url, 'Master')
+  });
+});
